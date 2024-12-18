f6b84f1d933159c583d24399cffef70255d1f1cf
Shamima-Nasrin10 <shamima48nasrin@gmail.com>
2024-12-18 09:45:07 +0600

    Spring Boot

---
+++ Patient-Registration-Backend/src/main/java/com/shamima/Patient_Registration_Backend/PatientRegistrationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.shamima.Patient_Registration_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientRegistrationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientRegistrationBackendApplication.class, args);
+	}
+
+}
---
+++ Patient-Registration-Backend/src/test/java/com/shamima/Patient_Registration_Backend/PatientRegistrationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shamima.Patient_Registration_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientRegistrationBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
